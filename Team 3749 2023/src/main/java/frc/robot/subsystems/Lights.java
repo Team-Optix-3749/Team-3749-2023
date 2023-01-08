2ce246e3d4d980dc178370d625ee74855bb030ce
SketchyBoi <whirlwindfury21@gmail.com>
2023-01-07 16:07:37 -0800

    fixed an issue with adressable led length

--- Team 3749 2023/src/main/java/frc/robot/subsystems/Lights.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/Lights.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.utils.Constants;
 import frc.robot.RobotContainer;
@@ -14,6 +15,6 @@ public class Lights extends SubsystemBase {
 
     public Lights() {
         AddressableLED m_led = new AddressableLED(Constants.Lights.led_port);
-        AddressableLED m_ledBuffer = new AddressableLEDBuffer(60);
+        AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(Constants.Lights.led_length);
     }
 }
