a3f1ebcbdb35c0bf6676e8d9d4067388d112ca06
horizon-ab <baileyasay@gmail.com>
2023-01-07 16:40:15 -0800

    hopefully blinking works

--- Team 3749 2023/src/main/java/frc/robot/commands/LightsCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/LightsCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.utils.Constants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -17,9 +18,12 @@ public class LightsCommand extends CommandBase {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
     Lights lights = new Lights();
+    Timer timer = new Timer();
 
     // Initializes the BaseCommand
     public LightsCommand(Lights lights) {
+    
+        timer.start();
     }
 
     // Run on command init
@@ -30,12 +34,17 @@ public class LightsCommand extends CommandBase {
   
     @Override
     public void execute() {
+        if (timer.get() % 1000 <= 20) {
+            lights.setRGBOfStrip(255, 255, 255, 0, 255, 0);
+            timer.reset();
+        }
     }
      
 
     // Run on command finish
     @Override
     public void end(boolean interrupted) {
+        timer.stop();
     }
 
     // Returns true when the command should end
