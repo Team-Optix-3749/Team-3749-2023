f088feff6c85d948a54dd877b7c4160b8f5ef4d9
KinetekEnergy <transyt8@gmail.com>
2023-01-10 17:57:21 -0800

    ClawCommand and Claw
    - Added ClawCommand.java
    - Added Claw.java
    --> added right side and left side motors as per claw CAD

---
+++ Team 3749 2023/src/main/java/frc/robot/commands/ClawCommand.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import frc.robot.utils.Constants;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/***
+ * @author Rohin Sood
+ * 
+ *         Serves as a template to format commands
+ */
+public class ClawCommand extends CommandBase {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+
+    Base base = new Base();
+
+    // Initializes the ClawCommand
+    public ClawCommand(Base base) {
+        this.base = base;
+        addRequirements(base);
+    }
+
+    // Run on command init
+    @Override
+    public void initialize() {
+    }
+
+    // Run every 20 ms
+    @Override
+    public void execute() {
+        base.set(Constants.Base.speed.get().doubleValue()); //set speed
+    }
+
+    // Run on command finish
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
--- Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
@@ -11,5 +11,10 @@ public final class Constants {
         public static final int neo_id = 3749;
         public static final int falcon_id = 6328;
         public static final SmartData<Double> speed = new SmartData<Double>("Base Speed", 2.54);
+
     }
+
+    public static final int right_side = 0; // right side of the claw (the motor)
+    public static final int left_side = 0; // left side of the claw (the motor)
+
 }
