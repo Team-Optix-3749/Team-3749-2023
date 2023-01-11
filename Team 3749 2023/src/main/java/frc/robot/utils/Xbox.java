ff6d139e3a4da0b16690a8b1e19eabfd9b2a7cdd
spooketti <jonliu168@gmail.com>
2023-01-10 18:09:31 -0800

    fixed one error

--- Team 3749 2023/src/main/java/frc/robot/utils/Xbox.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/Xbox.java
@@ -1,6 +1,6 @@
 package frc.robot.utils;
 
-import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi  .first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
