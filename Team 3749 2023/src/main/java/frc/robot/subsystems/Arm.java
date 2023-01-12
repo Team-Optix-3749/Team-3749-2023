a03fe428a2bbb72fa6dbdef0279471509433f385
AdityaSamavedam <adityasam1357@gmail.com>
2023-01-11 16:08:25 -0800

    fixed some conflicts

--- Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
@@ -13,20 +13,21 @@ import frc.robot.utils.Constants;
  */
 
 public class Arm extends SubsystemBase {
-    private CANSparkMax neoMotorTelescope = new CANSparkMax(Constants.Arm.neo_motor_telescope_port, MotorType.kBrushless); // Check if this is actually brushless later
-    private CANSparkMax neoMotorElevator = new CANSparkMax(Constants.Arm.neo_motor_elevator_port, MotorType.kBrushless); // Check if this is actually brushless later
+    
+    private CANSparkMax neo_motor_telescope = new CANSparkMax(Constants.Arm.neo_motor_telescope_port, MotorType.kBrushless); // Check if this is actually brushless later
+    private CANSparkMax neo_motor_elevator = new CANSparkMax(Constants.Arm.neo_motor_elevator_port, MotorType.kBrushless); // Check if this is actually brushless later
 
     public Arm() {
 
     }
 
-    public void setSpeedTelescope(double speed) {
-        neoMotorTelescope.set(speed);
-    }
-
     public void setSpeedElevator(double speed) {
-        neoMotorElevator.set(speed);
+        neo_motor_elevator.set(speed);
     }
 
+    public void setSpeedTelescope(double speed) {
+        neo_motor_telescope.set(speed);
+    }
+    
     
 }
