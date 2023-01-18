fc7592e38a991cf02fb8c567518ddd8f156cfefd
AdityaSamavedam <adityasam1357@gmail.com>
2023-01-17 16:39:26 -0800

    bark

--- Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
@@ -1,16 +1,10 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.Constants;
 
@@ -21,59 +15,25 @@ import frc.robot.utils.Constants;
  */
 
 public class Arm extends SubsystemBase {
-    // Motors, PIDS, Encoders, and other variables & instantiations 
-    private CANSparkMax lowerNeoMotor = new CANSparkMax(Constants.Arm.neo_motor_lower_port, MotorType.kBrushless); // Check if this is actually brushless later
-    private CANSparkMax upperNeoMotor = new CANSparkMax(Constants.Arm.neo_motor_upper_port, MotorType.kBrushless); // Check if this is actually brushless later
     
-    // level (temporary might change later)
-    private int level = 0;
+    private CANSparkMax neo_motor_lower_left = new CANSparkMax(Constants.Arm.neo_motor_lower_left_port, MotorType.kBrushless); // Check if this is actually brushless later
+    private CANSparkMax neo_motor_lower_right = new CANSparkMax(Constants.Arm.neo_motor_lower_right_port, MotorType.kBrushless); // Check if this is actually brushless later
 
-    private final DCMotor armGearbox = DCMotor.getNEO(Constants.Arm.number_of_motors);
-        
-    // Standard classes for controlling our arm
-    private final ProfiledPIDController topController = new ProfiledPIDController(Constants.Arm.kp, Constants.Arm.ki, Constants.Arm.kd, new TrapezoidProfile.Constraints(Constants.Arm.max_velocity, Constants.Arm.max_acceleration));
-    private final ProfiledPIDController bottomController = new ProfiledPIDController(Constants.Arm.kp, Constants.Arm.ki, Constants.Arm.kd, new TrapezoidProfile.Constraints(Constants.Arm.max_velocity, Constants.Arm.max_acceleration));
-    
-    // Relative Encoders
-    private final RelativeEncoder topEncoder = upperNeoMotor.getEncoder();
-    private final RelativeEncoder bottomEncoder = lowerNeoMotor.getEncoder();
+    private CANSparkMax neo_motor_upper = new CANSparkMax(Constants.Arm.neo_motor_upper_port, MotorType.kBrushless); // Check if this is actually brushless later
 
-    // Simulation Code
-    private final SingleJointedArmSim arm_top_sim = new SingleJointedArmSim(armGearbox, Constants.Simulation.arm_reduction, SingleJointedArmSim.estimateMOI(Constants.Simulation.arm_top_length, Constants.Simulation.arm_top_mass),
-            Constants.Simulation.arm_top_length,
-            Units.degreesToRadians(Constants.Simulation.arm_top_min_angle),
-            Units.degreesToRadians(Constants.Simulation.arm_top_max_angle),
-            Constants.Simulation.arm_top_mass,
-            false,
-            VecBuilder.fill(Constants.Simulation.arm_encoder_dist_per_pulse)); // Add noise with a std-dev of 1 tick);
+    private MotorControllerGroup neo_motor_lower = new MotorControllerGroup(neo_motor_lower_left, neo_motor_lower_right);
 
-    // Actual Arm Code
-    public Arm() {}
+    public Arm() {
+        neo_motor_lower_left.setInverted(true);
+    }
 
     public void setSpeedLower(double speed) {
-        lowerNeoMotor.set(speed);
+        neo_motor_lower.set(speed);
     }
 
     public void setSpeedUpper(double speed) {
-        upperNeoMotor.set(speed);
+        neo_motor_upper.set(speed);
     }
-
-    public boolean raiseLevel() {
-        if (level < 4) {
-            level += 1;
-            return false;
-        } //else {  We're already returning, the code ends there, however i'm leaving this here incase this leads to an error
-            return true;
-        //}
-    }
-
-    public boolean lowerLevel() {
-        if (level > 0) {
-            level -= 1;
-            return false;
-        } //else { Same reasoning as above 
-            return true;
-        //}
     
-    }
+    
 }
--- Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
@@ -1,7 +1,5 @@
 package frc.robot.utils;
 
-import edu.wpi.first.math.util.Units;
-
 /***
  * Stores constant variables within subclasses for different subsystems.
  * Such constant values can include motor IDs, motor speed, PID
@@ -20,71 +18,23 @@ public final class Constants {
         public static final int[] light1 = {0, 255, 0};
         public static final int[] light2 = {255, 255, 255};
     }
+    
+    public static final class Controller {
+        public static final int joystick_port = 0;
+    }
 
     public static final class Arm {
-        public static final int neo_motor_telescope_port = 13; // Change this value later
-        public static final int neo_motor_elevator_port = 15; // Change this value later
+        public static final int neo_motor_lower_left_port = 0; // Change this value later
+        public static final int neo_motor_lower_right_port = 1; //Change this value later
+        public static final int neo_motor_lower = 2;
+        public static final int neo_motor_upper_port = 3; // Change this value later
 
-        public static final int neo_motor_lower_port = 0; // Change this value later
-        public static final int neo_motor_upper_port = 1; // Change this value later
-
-        public static final int neo_motor_telescope_speed = 1; // Change this value later
-        public static final int neo_motor_elevator_speed = 1; // Change this value later
-
-        public static final int neo_motor_lower_speed = 1; // Change this value later
+        //public static final int neo_motor_lower_left_speed = 1; // Change this value later
+        //public static final int neo_motor_lower_right_speed = 1; //Change this value later
+        public static final int neo_motor_lower_speed = 1;
         public static final int neo_motor_upper_speed = 1; // Change this value later
 
         public static final int neo_motor_lower_stop = 0; // Change this value later
         public static final int neo_motor_upper_stop = 0; // Change this value later
-
-        public static final int number_of_motors = 2;
-
-        public static final int kp = 40;
-        public static final int ki = 0;
-        public static final int kd = 0;
-
-        public static final int max_velocity = 2;
-        public static final int max_acceleration = 5;
-    }
-
-    public static final class Simulation { // these values are going to need to change (along with simulation)
-        // distance per pulse = (angle per revolution) / (pulses per revolution)
-        //  = (2 * PI rads) / (4096 pulses)
-        public static final double arm_encoder_dist_per_pulse = 2.0 * Math.PI / 4096;
-        
-        // Simulation classes help us simulate what's going on, including gravity.
-        public static final double arm_reduction = 600;
-        public static final double arm_top_mass = 10.0; // Kilograms
-        public static final double arm_top_length = Units.inchesToMeters(38.5);
-        public static final double arm_bottom_mass = 4.0; // Kilograms
-        public static final double arm_bottom_length = Units.inchesToMeters(27);
-
-        public static final int arm_top_min_angle = -75; 
-        public static final int arm_top_max_angle = 260; 
-        public static final int arm_bottom_min_angle = 30; 
-        public static final int arm_bottom_max_angle = 150; 
-
-        //SETPOINTS FOR PRESETS MODE (Uses Virtual 4 Bar Mode for smooth movement)
-        public static final int stowed_bottom = 90;
-        public static final int stowed_top = 260;
-
-        public static final int intake_bottom = 135;
-        public static final int intake_top = 265;
-
-        public static final int double_substation_bottom = 60;
-        public static final int double_substation_top = 185;
-
-        public static final int score_floor_bottom = 120;
-        public static final int scoreFloorTop = 255;
-
-        public static final int score_mid_bottom = 95;
-        public static final int scoreMidTop = 195;
-
-        public static final int score_high_bottom = 135;
-        public static final int score_high_top = 160;
-    }
-
-    public static final class Controller {
-        public static final int joystick_port = 0;
     }
 }
