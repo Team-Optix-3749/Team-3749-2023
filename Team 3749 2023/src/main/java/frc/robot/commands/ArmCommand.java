fa4998260c59faf1561d82acaec93c548a99fc83
Don Tran <donqt15@gmail.com>
2023-01-21 11:53:51 -0800

    removed unused parts of ArmCommand

--- Team 3749 2023/src/main/java/frc/robot/commands/ArmCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/ArmCommand.java
@@ -28,27 +28,19 @@ public class ArmCommand extends CommandBase {
     // Run on command init
     @Override
     public void initialize() {
-        arm.setSpeedLower(0);
-        arm.setSpeedUpper(0);
+        
     }
 
     // Run every 20 ms
     @Override
     public void execute() { // we will figure this out later
-        //Base.set(Constants.Base.speed.get().doubleValue());
-        //neo motor speed isn't a constant yet
-
-        arm.setSpeedUpper(0);
-        // I have no idea what this code is trying to do, please be careful when running it
-        // arm.setSpeedUpper(Constants.Arm.neo_motor_lower_speed*-1);
-        // arm.setSpeedLower(Constants.Arm.neo_motor_lower_speed*-1);
+        
     }
 
     // Run on command finish
     @Override
     public void end(boolean interrupted) {
-        arm.setSpeedLower(0);
-        arm.setSpeedUpper(0);
+        
     }
 
     // Returns true when the command should end
