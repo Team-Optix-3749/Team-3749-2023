7849540bbed9961005ac2709d231e6d127c9224c
Don Tran <donqt15@gmail.com>
2023-01-21 11:36:55 -0800

    set follower motors, scuffed conversion factor

--- Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
@@ -26,10 +26,10 @@ public class Arm extends SubsystemBase {
 
     // set leaders and followers using .follow() in the constructor so you can
     // control the encoders of both motor controllers
-    private MotorControllerGroup upperMotorControllerGroup = new MotorControllerGroup(leftBicepMotor, rightBicepMotor,
-            null);
-    private MotorControllerGroup lowerMotorControllerGroup = new MotorControllerGroup(leftForearmMotor,
-            rightForearmMotor, null);
+    // private MotorControllerGroup upperMotorControllerGroup = new MotorControllerGroup(leftBicepMotor, rightBicepMotor,
+    //         null);
+    // private MotorControllerGroup lowerMotorControllerGroup = new MotorControllerGroup(leftForearmMotor,
+    //         rightForearmMotor, null);
 
     // Standard classes for controlling our arm
     // Not sure if same values for k values from Constants.Arm should be used for
@@ -46,8 +46,13 @@ public class Arm extends SubsystemBase {
     private final RelativeEncoder rightForearmEncoder = rightBicepMotor.getEncoder();
 
     public Arm() {
+        // inverted right motors
         rightForearmMotor.setInverted(true);
         rightBicepMotor.setInverted(true);
+
+        // right motors are follower motors for left motors
+        rightBicepMotor.follow(leftBicepMotor);
+        rightForearmMotor.follow(leftBicepMotor);
 
         // conversion factor is ((gear ratio)/(encoder resolution) * 360) degrees
         leftBicepEncoder.setPositionConversionFactor(250/2048*360);
@@ -66,26 +71,34 @@ public class Arm extends SubsystemBase {
     // PID + feedforward implementation; should return the needed voltage, need to
     // do feedforward
     // desired posiiton and make sure position values are good for both
-    public void setForearmVoltage(double x, double y) {
+    // public void setForearmVoltage(double x, double y) {
 
-        // this sets voltage to degrees (not good)
-        upperMotorControllerGroup.setVoltage(
-                forearmController.calculate(leftForearmEncoder.getPosition(),
-                        BruteInverseKinematics.calculate(x, y)[0]));
-        // taken from wpilib documentation: not too sure how this all works yet
+    //     // this sets voltage to degrees (not good)
+    //     upperMotorControllerGroup.setVoltage(
+    //             forearmController.calculate(leftForearmEncoder.getPosition(),
+    //                     BruteInverseKinematics.calculate(x, y)[0]));
+    //     // taken from wpilib documentation: not too sure how this all works yet
+    // }
+
+    // public void setBicepVoltage(double x, double y) {
+
+    //     // this sets voltage to degrees (not good)
+    //     lowerMotorControllerGroup.setVoltage(
+    //             bicepController.calculate(leftBicepEncoder.getPosition(), BruteInverseKinematics.calculate(x, y)[1]));
+    //     // index idk if we want to clean this up lmao
+    // }
+
+    // public void setDegreesUpper(double anlge) {
+    //     // NEED PID TO CONTROL ACCURATELY
+    //     // would work if the conversion factor was correctly set
+    //     leftBicepEncoder.setPosition(anlge);
+    // }
+
+    public void setForearmPosition(){
+
     }
 
-    public void setBicepVoltage(double x, double y) {
+    public void setBicepPosition(){
 
-        // this sets voltage to degrees (not good)
-        lowerMotorControllerGroup.setVoltage(
-                bicepController.calculate(leftBicepEncoder.getPosition(), BruteInverseKinematics.calculate(x, y)[1]));
-        // index idk if we want to clean this up lmao
-    }
-
-    public void setDegreesUpper(double anlge) {
-        // NEED PID TO CONTROL ACCURATELY
-        // would work if the conversion factor was correctly set
-        leftBicepEncoder.setPosition(anlge);
     }
 }
