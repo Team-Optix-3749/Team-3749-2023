0e6960e337074229a2fd0899ce5280ac3f2c2f4b
KinetekEnergy <transyt8@gmail.com>
2023-01-24 18:10:09 -0800

    added basic code to clawSim and clawSimCMD
    clawSimCMD:
    - added some objects and MCG for clawSimCMD
    
    clawSim:
    - comments
    - removed redundant code (from base.java)

--- Team 3749 2023/src/main/java/frc/robot/commands/ClawSimulationCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/ClawSimulationCommand.java
@@ -6,12 +6,28 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.*;
 import frc.robot.utils.Constants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ClawSimulationCommand extends CommandBase {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
     ClawSim clawSim = new ClawSim();
+
+    // left and right side of the claw (the motor)
+    private CANSparkMax right_motor = new CANSparkMax(Constants.Claw.right_side, MotorType.kBrushless);
+    private CANSparkMax left_motor = new CANSparkMax(Constants.Claw.left_side, MotorType.kBrushless);
+
+    // motor controller group for both sides
+    private MotorControllerGroup clawMotors = new MotorControllerGroup(left_motor, right_motor);
+
+    // relative encoder
+    private final RelativeEncoder claw_encoder = right_motor.getEncoder();
 
     // Initializes the ClawSimulationCommand
     public ClawSimulationCommand(ClawSim clawSim) {
--- Team 3749 2023/src/main/java/frc/robot/subsystems/ClawSim.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/ClawSim.java
@@ -4,22 +4,32 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.Constants;
 
 public class ClawSim extends SubsystemBase {
+    /*
+     * distance per pulse = (angle per revolution) / (pulses per revolution)
+     * = (2 * PI rads) / (4096 pulses)
+     * 
+     * DPP = dist per pulse
+     */
+
+	private static final double encoder_DPP = (2.0 * Math.PI) / 4096;
+
+    // Creates a PIDController with gains kP, kI, and kD
+    PIDController claw_PID = new PIDController(Constants.Claw.claw_kP, Constants.Claw.claw_kI, Constants.Claw.claw_kD);
 
     private CANSparkMax neo = new CANSparkMax(Constants.Base.neo_id, MotorType.kBrushless);
-    private WPI_TalonFX falcon = new WPI_TalonFX(Constants.Base.falcon_id);
 
-    private MotorControllerGroup base = new MotorControllerGroup(neo, falcon);
+    private MotorControllerGroup base = new MotorControllerGroup(neo);
 
     // Initializes the clawSim subsystem
     public ClawSim() {
-        neo.setInverted(true);  
-        falcon.setNeutralMode(NeutralMode.Brake);
-
+        neo.setInverted(true);
         //Constants.Base.speed.set(new Double(16.90));
     }
 
