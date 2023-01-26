b2e1562b70406bf65abb067a93e5af27c0fa9a8a
Harkirat47 <har.ki.kirat@gmail.com>
2023-01-25 17:14:22 -0800

    revert back, you dont need all of claw

--- Team 3749 2023/src/main/java/frc/robot/subsystems/ClawSim.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/ClawSim.java
@@ -1,12 +1,9 @@
-//Claw.java creates objects, dependencies, and motor controller groups
-//to allow us to set the speed of each motor for intake and outtake
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -14,49 +11,50 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.Constants;
 
 public class ClawSim extends SubsystemBase {
+    /*
+     * distance per pulse = (angle per revolution) / (pulses per revolution)
+     * = (2 * PI rads) / (4096 pulses)
+     * 
+     * DPP = dist per pulse
+     */
+
+	private static final double encoder_DPP = (2.0 * Math.PI) / 4096;
+
     // Creates a PIDController with gains kP, kI, and kD
     PIDController claw_PID = new PIDController(Constants.Claw.claw_kP, Constants.Claw.claw_kI, Constants.Claw.claw_kD);
 
-    // left and right side of the claw (the motor)
-    private CANSparkMax right_motor = new CANSparkMax(Constants.Claw.right_side, MotorType.kBrushless);
-    private CANSparkMax left_motor = new CANSparkMax(Constants.Claw.left_side, MotorType.kBrushless);
+    private CANSparkMax neo = new CANSparkMax(Constants.Base.neo_id, MotorType.kBrushless);
 
-    // motor controller group for both sides
-    private MotorControllerGroup clawMotors = new MotorControllerGroup(left_motor, right_motor);
+    private MotorControllerGroup base = new MotorControllerGroup(neo);
 
-    // relative encoder
-    private final RelativeEncoder claw_encoder = right_motor.getEncoder();
-
-    // Initializes the base subsystem
+    // Initializes the clawSim subsystem
     public ClawSim() {
-        right_motor.setInverted(true); // invert the motor to not break it
-
-        right_motor.setIdleMode(IdleMode.kBrake); // set neo to be braked when not active
-        left_motor.setIdleMode(IdleMode.kBrake); // set neo to be braked when not active
-
-        // not sure what this is: Constants.Base.speed.set(new Double(16.90));
+        neo.setInverted(true);
+        //Constants.Base.speed.set(new Double(16.90));
     }
 
-    /**
-     * set speed for motor
+    /***
+     * Sets the speed. Value is between -1.0 and 1.0
      * 
-     * @param speed
+     * @param percent_speed
      */
+    public void set(double percent_speed) {
+        base.set(percent_speed);
+    }
 
-    public void setSpeed(double speed) {
-        clawMotors.set(speed);
+    /***
+     * Gets the speed. Value is between -1.0 and 1.0
+     * 
+     * @return speed of the first motor in the motor controller group (neo)
+     */
+    public double get() {
+        return base.get();
     }
 
     // Runs every 20 ms
     @Override
     public void periodic() {
-        /*
-         * Calculates the output of the PID algorithm based on the sensor reading
-         * sends it to a motor
-         * uses calculate()
-         */
-        right_motor.set(claw_PID.calculate(claw_encoder.getPosition(), Constants.Claw.setpoint));
-        left_motor.set(claw_PID.calculate(claw_encoder.getPosition(), Constants.Claw.setpoint));
+
     }
 
 }
