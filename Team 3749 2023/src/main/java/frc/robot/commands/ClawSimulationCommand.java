e9021ae5eeea216629a39d4a6b8b42952ba818ff
Harkirat47 <har.ki.kirat@gmail.com>
2023-01-28 10:21:21 -0800

    many sim vendordepts

--- Team 3749 2023/src/main/java/frc/robot/commands/ClawSimulationCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/ClawSimulationCommand.java
@@ -1,59 +1,59 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import frc.robot.subsystems.*;
-// import frc.robot.utils.Constants;
+import frc.robot.subsystems.*;
+import frc.robot.utils.Constants;
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-// import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 
-// import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-// import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-// public class ClawSimulationCommand extends CommandBase {
-//     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+public class ClawSimulationCommand extends CommandBase {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
-//     ClawSim clawSim = new ClawSim();
+    ClawSim clawSim = new ClawSim();
 
-//     // left and right side of the claw (the motor)
-//     private CANSparkMax right_motor = new CANSparkMax(Constants.Claw.right_side, MotorType.kBrushless);
-//     private CANSparkMax left_motor = new CANSparkMax(Constants.Claw.left_side, MotorType.kBrushless);
+    // left and right side of the claw (the motor)
+    private CANSparkMax right_motor = new CANSparkMax(Constants.Claw.right_side, MotorType.kBrushless);
+    private CANSparkMax left_motor = new CANSparkMax(Constants.Claw.left_side, MotorType.kBrushless);
 
-//     // motor controller group for both sides
-//     private MotorControllerGroup clawMotors = new MotorControllerGroup(left_motor, right_motor);
+    // motor controller group for both sides
+    private MotorControllerGroup clawMotors = new MotorControllerGroup(left_motor, right_motor);
 
-//     // relative encoder
-//     private final RelativeEncoder claw_encoder = right_motor.getEncoder();
+    // relative encoder
+    private final RelativeEncoder claw_encoder = right_motor.getEncoder();
 
-//     // Initializes the ClawSimulationCommand
-//     public ClawSimulationCommand(ClawSim clawSim) {
-//         this.clawSim = clawSim;
-//         addRequirements(clawSim);
-//     }
+    // Initializes the ClawSimulationCommand
+    public ClawSimulationCommand(ClawSim clawSim) {
+        this.clawSim = clawSim;
+        addRequirements(clawSim);
+    }
 
-//     // Run on command init
-//     @Override
-//     public void initialize() {
-//     }
+    // Run on command init
+    @Override
+    public void initialize() {
+    }
 
-//     // Run every 20 ms
-//     @Override
-//     public void execute() {
-//         clawSim.set(Constants.Base.speed.get().doubleValue());
-//     }
+    // Run every 20 ms
+    @Override
+    public void execute() {
+        clawSim.setSpeed(Constants.Base.speed.get().doubleValue());
+    }
 
-//     // Run on command finish
-//     @Override
-//     public void end(boolean interrupted) {
-//     }
+    // Run on command finish
+    @Override
+    public void end(boolean interrupted) {
+    }
 
-//     // Returns true when the command should end
-//     @Override
-//     public boolean isFinished() {
-//         return false;
-//     }
-// }
+    // Returns true when the command should end
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
