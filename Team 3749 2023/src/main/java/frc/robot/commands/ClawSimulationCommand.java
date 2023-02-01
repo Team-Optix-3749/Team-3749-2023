50861b8f7dcd54f2e8b6973f15f5515c323e3731
KinetekEnergy <transyt8@gmail.com>
2023-01-31 16:08:18 -0800

    comments ;)

--- Team 3749 2023/src/main/java/frc/robot/commands/ClawSimulationCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/ClawSimulationCommand.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 /***
- * @author Harkirat 
+ * @author Zaddy Harkirat 
  * @author Anusha Khobare
  * @author Aashray Reddy
  * @author Ryan R McWeeny
