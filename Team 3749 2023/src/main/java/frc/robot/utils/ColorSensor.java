fb5ff3a9e6126fcac7afee51d6191dbdf93de5f5
KinetekEnergy <transyt8@gmail.com>
2023-02-07 17:00:37 -0800

    fixing harkirats mess (still)

--- Team 3749 2023/src/main/java/frc/robot/utils/ColorSensor.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/ColorSensor.java
@@ -1,14 +1,5 @@
 package frc.robot.utils;
 
-/***
- * @author Anusha Khobare
- * @author Ryan R McWeeny
- * 
- *     ColorSensor.java is color sensor code to differniate between the yellow cone and purple cube gamepieces.
- *     Note that this ultility is currently unused in claw code. But is ready for possible future use (use the gamePiece Function).
- *     Rearranged + Adapted code from RevRobotics Color Sensor Template Code
- */
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
@@ -20,11 +11,19 @@ import com.revrobotics.ColorMatch;
 import frc.robot.subsystems.Claw;
 import frc.robot.utils.Constants;
 
+/***
+ * @author Anusha Khobare
+ * @author Ryan R McWeeny
+ * 
+ *     ColorSensor.java is color sensor code to differniate between the yellow cone and purple cube gamepieces.
+ *     Note that this ultility is currently unused in claw code. But is ready for possible future use (use the gamePiece Function).
+ *     Rearranged + Adapted code from RevRobotics Color Sensor Template Code
+ */
 public class ColorSensor {
     // defines color sensor
-    private final I2C.Port i2cPort = I2C.Port.kOnboard;
-    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-    private final ColorMatch m_colorMatcher = new ColorMatch();
+    private final static I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final static ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+    private final static ColorMatch m_colorMatcher = new ColorMatch();
 
     public Claw claw; //object
 
@@ -34,16 +33,19 @@ public class ColorSensor {
         m_colorMatcher.addColorMatch(Constants.cube_color);
     }
 
-    // Constants.Claw.Object is a String (see Constants.java)
-    // States object obtained (possible objects: "Cone", "Cube", None)
-    // is in the claw (returned in "gamePiece" method)
+    /**
+     * uses the color sensor and finds what object is in the claw currently
+     * this is done by matching the object's detected color to a color stored in Constants.java
+     * returns the game piece that it thinks is in the claw
+     * @return
+     */
     public static String gamePiece(){
         // .getColor gets RGB values at the current time (what the sensor sees)
         Color detectedColor = m_colorSensor.getColor();
         
         //.matchClosestColor() caluclates the closest color from the listed colors in Color Matcher array
         ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
-        Smartdashboard.
+        
         if (match.color == Constants.cone_color) {
             //If cone_color detected, it is cone
             Constants.Claw.Object = "Cone";
