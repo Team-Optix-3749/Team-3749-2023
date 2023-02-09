7fdfaaac5dc7f5c1a8adea12dde83af5164f8afc
KinetekEnergy <transyt8@gmail.com>
2023-02-08 16:28:50 -0800

    formatting

--- Team 3749 2023/src/main/java/frc/robot/commands/ClawAutoCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/ClawAutoCommand.java
@@ -11,46 +11,46 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 /***
  * @author Harkirat Hattar
  * @author Aashray Reddy
- * Command for the auto function of the claw  
+ *         Command for the auto function of the claw
  * 
  * 
  * 
  * 
  * 
- * extra space wow look here rohin does this anger you?
+ *         extra space wow look here rohin does this anger you?
  */
 public class ClawAutoCommand extends CommandBase {
-    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
-    Claw claw;
+  Claw claw;
 
-    // Initializes the ClawAutoCommand
-    public ClawAutoCommand(Claw Claw) {
-      this.claw = Claw;
-      addRequirements(Claw);
-    }
+  // Initializes the ClawAutoCommand
+  public ClawAutoCommand(Claw Claw) {
+    this.claw = Claw;
+    addRequirements(Claw);
+  }
 
-    // Run on command init
-    @Override
-    public void initialize() {
-    }
+  // Run on command init
+  @Override
+  public void initialize() {
+  }
 
-    // Run every 20 ms
-    @Override
-    public void execute() {
-      // uses PID to calculate the velocity needed to acheive an exact speed
-      claw.setSpeed(-Constants.Claw.setpoint_velocity);
-    }
+  // Run every 20 ms
+  @Override
+  public void execute() {
+    // uses PID to calculate the velocity needed to acheive an exact speed
+    claw.setSpeed(-Constants.Claw.setpoint_velocity);
+  }
 
-    // Run on command finish
-    @Override
-    public void end(boolean interrupted) {
-      //claw.setSpeed(Constants.Claw.stop); //set speed to 0 (stop)
-    }
+  // Run on command finish
+  @Override
+  public void end(boolean interrupted) {
+    // claw.setSpeed(Constants.Claw.stop); //set speed to 0 (stop)
+  }
 
-    // Returns true when the command should end
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  // Returns true when the command should end
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
