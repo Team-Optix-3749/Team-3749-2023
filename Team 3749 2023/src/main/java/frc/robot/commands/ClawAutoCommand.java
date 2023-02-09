eaf75910d8d1d7409ddfaea29f71bc55f80ebd86
KinetekEnergy <transyt8@gmail.com>
2023-02-08 16:24:44 -0800

    added bp to CAC

--- Team 3749 2023/src/main/java/frc/robot/commands/ClawAutoCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/ClawAutoCommand.java
@@ -1,8 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
+import frc.robot.subsystems.*;
+import frc.robot.utils.Constants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/***
+ * @author Rohin Sood
+ * 
+ *         Serves as a template to format commands
+ */
 public class ClawAutoCommand extends CommandBase {
-  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" });
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
+    Claw claw;
+
+    // Initializes the ClawAutoCommand
+    public ClawAutoCommand(Claw Claw) {
+        this.claw = Claw;
+        addRequirements(Claw);
+    }
+
+    // Run on command init
+    @Override
+    public void initialize() {
+    }
+
+    // Run every 20 ms
+    @Override
+    public void execute() {
+        base.set(Constants.Base.speed.get().doubleValue());
+    }
+
+    // Run on command finish
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
