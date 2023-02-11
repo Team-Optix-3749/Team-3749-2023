a2de88b6063015eb308245ebc406894d605560d3
rohin <rs.rohinsood@gmail.com>
2023-02-11 00:02:12 -0800

    refactoring& formatting changes

--- Team 3749 2023/src/main/java/frc/robot/commands/ArmTeleopCommand.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/ArmTeleopCommand.java
@@ -1,14 +1,7 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
-import frc.robot.utils.Constants.Arm.*;
-import frc.robot.utils.Constants;
 import frc.robot.utils.Xbox;
 // import frc.robot.utils.ArmSim;
 
@@ -18,13 +11,6 @@ public class ArmTeleopCommand extends CommandBase {
   private final Arm arm;
 
   private final Xbox operator;
-
-  private ProfiledPIDController elbowController = new ProfiledPIDController(Constants.Arm.elbowKP.get(),
-  Constants.Arm.elbowKI.get(), Constants.Arm.elbowKD.get(),
-  new TrapezoidProfile.Constraints(2, 5));
-  private ProfiledPIDController shoulderController = new ProfiledPIDController(Constants.Arm.shoulderKP.get(),
-  Constants.Arm.shoulderKI.get(), Constants.Arm.shoulderKD.get(),
-      new TrapezoidProfile.Constraints(2, 5));
 
   public ArmTeleopCommand(Arm armSim, Xbox operator) {
     this.arm = armSim;
@@ -38,61 +24,11 @@ public class ArmTeleopCommand extends CommandBase {
   
   @Override
   public void execute() {
+    double shoulderVoltage = 12 * operator.getRightY();
+    double elbowVoltage = 12 * operator.getLeftY();
     
-    // switch (Constants.Arm.controlMode.getSelected()) {
-      //   case 1:
-      
-      double shoulderVoltage = 12 * operator.getRightY();
-      double elbowVoltage = 12 * operator.getLeftY();
-      
-      arm.setShoulderVoltage(shoulderVoltage);
-      arm.setElbowVoltage(elbowVoltage);
-
-  //       break;
-  //     default: // also case 0
-
-  //       double elbowSetpoint, shoulderSetpoint;
-
-  //       if (operator.getLeftTrigger()) { // cube setpoints
-  //         if (operator.getRawButton(Button.kX.value)) { // mid
-  //           elbowSetpoint = 0;
-  //           shoulderSetpoint = 0;
-  //         }
-  //         if (operator.getRawButton(Button.kY.value)) { // high
-  //           elbowSetpoint = 0;
-  //           shoulderSetpoint = 0;
-  //         }
-  //       } else if (operator.getRightTrigger()) { // cone setpoints
-  //         if (operator.getRawButton(Button.kX.value)) { // mid
-  //           elbowSetpoint = 0;
-  //           shoulderSetpoint = 0;
-  //         }
-  //         if (operator.getRawButton(Button.kY.value)) { // high
-  //           elbowSetpoint = 0;
-  //           shoulderSetpoint = 0;
-  //         }
-  //       } else if (operator.getRawButton(Button.kA.value)) { // floor
-  //         elbowSetpoint = 0;
-  //         shoulderSetpoint = 0;
-  //       } else if (operator.getRawButton(Button.kB.value)) { // stowing
-  //         elbowSetpoint = ElbowSetpoints.STOWED.angle;
-  //         shoulderSetpoint = ShoulderSetpoints.STOWED.angle;
-  //       } else {
-  //         switch (ArmSim.presetChooser.getSelected()) {
-  //           case 0:
-  //             elbowSetpoint = ElbowSetpoints.STOWED.angle;
-  //             shoulderSetpoint = ShoulderSetpoints.STOWED.angle;
-  //             break;
-  //           default:
-  //             elbowSetpoint = ElbowSetpoints.STOWED.angle;
-  //             shoulderSetpoint = ShoulderSetpoints.STOWED.angle;
-  //             break;
-  //         }
-
-  //         arm.setShoulderVoltage(pidOutputShoulder);
-  //       }
-  //       break;
-  //   }
+    arm.setShoulderVoltage(shoulderVoltage);
+    arm.setElbowVoltage(elbowVoltage);
   }
 
   @Override
--- Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/Arm.java
@@ -1,21 +1,13 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.REVPhysicsSim;
-import com.revrobotics.SparkMaxAbsoluteEncoder;
-import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.simulation.BatterySim;
-import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import frc.robot.utils.ArmSim;
 import frc.robot.utils.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -25,57 +17,30 @@ public class Arm extends SubsystemBase {
 	private final CANSparkMax rightShoulderMotor = new CANSparkMax(Constants.Arm.right_shoulder_id, MotorType.kBrushless);
 	private final DutyCycleEncoder shoulderAbsoluteEncoder = new DutyCycleEncoder(0);
 	private PIDController shoulderPIDController = new PIDController(0.008, 0, 0);
-	// private final AbsoluteEncoder shoulderAbsoluteEncoder = leftShoulderMotor.getAbsoluteEncoder(Type.kDutyCycle);
-	
+
 	private final CANSparkMax leftElbowMotor = new CANSparkMax(Constants.Arm.left_elbow_id, MotorType.kBrushless);
 	private final CANSparkMax rightElbowMotor = new CANSparkMax(Constants.Arm.right_elbow_id, MotorType.kBrushless);
 	private final DutyCycleEncoder elbowAbsoluteEncoder = new DutyCycleEncoder(1);
 	private PIDController elbowPIDController = new PIDController(0.008, 0, 0);
-	// private final AbsoluteEncoder elbowAbsoluteEncoder = leftElbowMotor.getAbsoluteEncoder(Type.kDutyCycle);
 
-	// This arm sim represents an arm that can travel from -75 degrees (rotated down
-	// front)
-	// to 255 degrees (rotated down in the back).
+	private ProfiledPIDController elbowController = new ProfiledPIDController(Constants.Arm.elbowKP.get(),
+			Constants.Arm.elbowKI.get(), Constants.Arm.elbowKD.get(),
+			new TrapezoidProfile.Constraints(2, 5));
+	private ProfiledPIDController shoulderController = new ProfiledPIDController(Constants.Arm.shoulderKP.get(),
+			Constants.Arm.shoulderKI.get(), Constants.Arm.shoulderKD.get(),
+			new TrapezoidProfile.Constraints(2, 5));
 
 	public Arm() {
-		// rightElbowMotor.follow(leftElbowMotor);
 		leftShoulderMotor.restoreFactoryDefaults();
 		rightShoulderMotor.restoreFactoryDefaults();
 
 		leftElbowMotor.restoreFactoryDefaults();
 		rightElbowMotor.restoreFactoryDefaults();
-
-		// rightElbowMotor.setInverted(true);
-		// rightShoulderMotor.setInverted(true);
-
-		// SmartDashboard.putNumber("Setpoint top (degrees)", 90);
-		// SmartDashboard.putNumber("Setpoint bottom (degrees)", 90);
-
-		// Constants.Arm.controlMode.setDefaultOption("Joystick Control", 0);
-		// Constants.Arm.controlMode.addOption("Presets", 1);
-		// SmartDashboard.putData(Constants.Arm.controlMode);
-
-	// 	ArmSim.elbowEncoder.setDistancePerPulse(Constants.Arm.sim_encoder_dist_per_pulse);
-	// 	ArmSim.shoulderEncoder.setDistancePerPulse(Constants.Arm.sim_encoder_dist_per_pulse);
-
-  //   ArmSim.presetChooser.setDefaultOption("Starting Position", 0);
-  //   ArmSim.presetChooser.addOption("Floor Intake Position", 1);
-  //   ArmSim.presetChooser.addOption("High Node Score", 5);
-  //   SmartDashboard.putData(ArmSim.presetChooser);
-  //   // Put Mechanism 2d to SmartDashboard
-  //   SmartDashboard.putData("Arm Sim", ArmSim.mech2d);
 	}
-	
+
 	public void setElbowVoltage(double voltage) {
 		leftElbowMotor.setVoltage(voltage);
 	}
-
-	// public void setElbowPosition(double position) {
-
-	// }
-
-	// public void setShoulderPosition(double position) {
-	// }
 
 	public void setShoulderVoltage(double voltage) {
 		leftShoulderMotor.setVoltage(voltage);
@@ -95,7 +60,7 @@ public class Arm extends SubsystemBase {
 		// STOW: 0.601644465041112
 		return shoulderAbsoluteEncoder.getDistance();
 	}
-	
+
 	public double getElbowPosition() {
 		// STOW: 0.328055008201375
 		return elbowAbsoluteEncoder.getDistance();
@@ -103,63 +68,35 @@ public class Arm extends SubsystemBase {
 
 	public void setShoulderPosition(double position) {
 		leftShoulderMotor.set(-shoulderPIDController.calculate(
-			shoulderAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
+				shoulderAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
 		rightShoulderMotor.set(shoulderPIDController.calculate(
-			shoulderAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
+				shoulderAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
 
 		SmartDashboard.putNumber("Shoulder Setter PID", shoulderPIDController.calculate(
-			shoulderAbsoluteEncoder.getDistance() * 100, position * 100));
-			
+				shoulderAbsoluteEncoder.getDistance() * 100, position * 100));
+
 		SmartDashboard.putNumber("Shoulder Abs PID", shoulderAbsoluteEncoder.getDistance());
 	}
 
 	public void setElbowPosition(double position) {
 		leftElbowMotor.set(-elbowPIDController.calculate(
-			elbowAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
+				elbowAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
 		rightElbowMotor.set(elbowPIDController.calculate(
-			elbowAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
+				elbowAbsoluteEncoder.getDistance() * 100, position * 100) * 10);
 
 		SmartDashboard.putNumber("Elbow Setter PID", elbowPIDController.calculate(
-			elbowAbsoluteEncoder.getDistance() * 100, position * 100));
-			
+				elbowAbsoluteEncoder.getDistance() * 100, position * 100));
+
 		SmartDashboard.putNumber("Elbow Abs PID", elbowAbsoluteEncoder.getDistance());
 	}
 
 	@Override
 	public void simulationPeriodic() {
-		// REVPhysicsSim.getInstance().run();
-
-		// In this method, we update our simulation of what our arm is doing
-		// First, we set our "inputs" (voltages)
-		// ArmSim.elbowSim.setInput(leftElbowMotor.getAppliedOutput() * RobotController.getBatteryVoltage());
-		// ArmSim.shoulderSim.setInput(leftShoulderMotor.getAppliedOutput() * RobotController.getBatteryVoltage());
-		// // Next, we update it. The standard loop time is 20ms.
-		// ArmSim.elbowSim.update(0.020);
-		// ArmSim.shoulderSim.update(0.020);
-
-		// // Finally, we set our simulated encoder's readings and simulated battery
-		// // voltage
-		// ArmSim.elbowEncoderSim.setDistance(ArmSim.elbowSim.getAngleRads());
-		// ArmSim.shoulderEncoderSim.setDistance(ArmSim.shoulderSim.getAngleRads());
-		
-		// // SimBattery estimates loaded battery voltages
-		// RoboRioSim.setVInVoltage(
-		// 		BatterySim.calculateDefaultBatteryLoadedVoltage(
-		// 				ArmSim.elbowSim.getCurrentDrawAmps() + ArmSim.shoulderSim.getCurrentDrawAmps()));
-
-		// // Update the Mechanism Arm angle based on the simulated arm angle
-		// ArmSim.forearm.setAngle(Units.radiansToDegrees(ArmSim.elbowSim.getAngleRads()));
-		// ArmSim.bicep.setAngle(Units.radiansToDegrees(ArmSim.shoulderSim.getAngleRads()));
 	}
 
 	@Override
 	public void periodic() {
 		SmartDashboard.putNumber("Elbow Abs", getElbowPosition());
 		SmartDashboard.putNumber("Shoulder Abs", getShoulderPosition());
-
-		// SmartDashboard.putNumber("left elbow voltage", leftElbowMotor.getAppliedOutput() * leftElbowMotor.getBusVoltage());
-		// SmartDashboard.putNumber("left shoulder voltage", leftShoulderMotor.getAppliedOutput() * leftShoulderMotor.getBusVoltage());
-		// SmartDashboard.putNumber("right elbow voltage", rightElbowMotor.getAppliedOutput() * rightElbowMotor.getBusVoltage());
-		// SmartDashboard.putNumber("right shoulder voltage", rightShoulderMotor.getAppliedOutput() * rightShoulderMotor.getBusVoltage());
 	}
 }
