d2b5a1cc2d5ae97b2b6f3a8ad6ebccc87f0775b6
rohin <rs.rohinsood@gmail.com>
2023-02-11 09:43:53 -0800

    delete unused imports

--- Team 3749 2023/src/main/java/frc/robot/subsystems/arm/ArmReal.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/arm/ArmReal.java
@@ -4,8 +4,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.utils.Constants;
