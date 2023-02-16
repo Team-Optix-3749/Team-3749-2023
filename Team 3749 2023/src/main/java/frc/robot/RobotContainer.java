ca64083c237141a5410c1bd2607ea1e8b804effc
Raadwan Masum <piraadwan@gmail.com>
2023-02-15 20:43:18 -0800

    format robot container

--- Team 3749 2023/src/main/java/frc/robot/RobotContainer.java
+++ Team 3749 2023/src/main/java/frc/robot/RobotContainer.java
@@ -1,85 +1,79 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.*;
 import frc.robot.subsystems.Claw;
-import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.utils.Xbox;
-import frc.robot.Constants.OIConstants;
-import frc.robot.commands.AutoCommands;
-import frc.robot.commands.MoveDistance;
-import frc.robot.commands.SwerveTeleopCommand;
+import frc.robot.commands.*;
+import frc.robot.utils.*;
+import frc.robot.Constants.*;
 
 public class RobotContainer {
+    // Controllers
+    private final Xbox pilot = new Xbox(OIConstants.kDriverControllerPort);
+    private final Xbox operator = new Xbox(1);
 
-  // Controllers
-  private final Xbox pilot = new Xbox(OIConstants.kDriverControllerPort);
-  private final Xbox operator = new Xbox(1);
+    // Subsystems
+    private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+    private final Claw clawSubsystem = new Claw();
 
-  // private final POV pilotPOV = new POV(pilot);
-  // private final POV operatorPOV = new POV(operator);
+    // Commands
+    private final MoveDistance moveDistance = new MoveDistance(swerveSubsystem, Units.feetToMeters(5));
 
-  // Subsystems
-  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-  private final Arm armSubsystem = new Arm();
-  private final Claw clawSubsystem = new Claw();
+    public RobotContainer() {
+        setupAuto();
+        configureButtonBindings();
+        configureDefaultCommands();
 
-  // Commands
-  private final MoveDistance moveDistance = new MoveDistance(swerveSubsystem, Units.feetToMeters(5));
+        swerveSubsystem.setDefaultCommand(new SwerveTeleopCommand(
+                swerveSubsystem,
+                () -> -pilot.getLeftY(),
+                () -> pilot.getLeftX(),
+                () -> pilot.getRightX()));
 
-  public RobotContainer() {
-    setupAuto();
-    configureButtonBindings();
-    configureDefaultCommands();
+        configureButtonBindings();
+    }
 
-    swerveSubsystem.setDefaultCommand(new SwerveTeleopCommand(
-        swerveSubsystem,
-        () -> -pilot.getLeftY(),
-        () -> pilot.getLeftX(),
-        () -> pilot.getRightX()));
+    /**
+     * Set default commands
+     */
+    private void configureDefaultCommands() {
+    }
 
-    configureButtonBindings();
-  }
+    /**
+     * Set controller button bindings
+     */
+    private void configureButtonBindings() {
+        pilot.a().whileTrue(new InstantCommand(swerveSubsystem::zeroHeading));
+        pilot.b().whileTrue(moveDistance);
+    }
 
-  private void configureDefaultCommands() {
-  }
+    /**
+     * @return Autonomous Command
+     */
+    public Command getAutonomousCommand() {
+        return AutoCommands.getTestPathPlanner(swerveSubsystem, Alliance.Blue);
+    }
 
-  private void configureButtonBindings() {
-    pilot.a().whileTrue(new InstantCommand(swerveSubsystem::zeroHeading));
-    pilot.b().whileTrue(moveDistance);
-
-  }
-
-  public Command getAutonomousCommand() {
-    return AutoCommands.getTestPathPlanner(swerveSubsystem, Alliance.Blue);
-  }
-
-  public void setupAuto(){
-    Constants.AutoConstants.eventMap.put("pickup_cone_floor", Commands.print("PICKUP CONE FLOOR"));
-    Constants.AutoConstants.eventMap.put("pickup_cube_floor", null);
-    Constants.AutoConstants.eventMap.put("pickup_cone_double_substation", null);
-    Constants.AutoConstants.eventMap.put("pickup_cube_double_substation", null);
-    Constants.AutoConstants.eventMap.put("pickup_cone_single_substation", null);
-    Constants.AutoConstants.eventMap.put("pickup_cube_single_substation", null);
-    Constants.AutoConstants.eventMap.put("place_cone_bottom", null);
-    Constants.AutoConstants.eventMap.put("place_cube_bottom", null);
-    Constants.AutoConstants.eventMap.put("place_cone_mid", null);
-    Constants.AutoConstants.eventMap.put("place_cube_mid", null);
-    Constants.AutoConstants.eventMap.put("place_cone_top", null);
-    Constants.AutoConstants.eventMap.put("place_cube_top", null);
-    Constants.AutoConstants.eventMap.put("run_claw", Commands.run(() -> clawSubsystem.set(0.2), clawSubsystem));
-
-
-  }
+    /**
+     * Set event maps for autonomous
+     */
+    public void setupAuto() {
+        Constants.AutoConstants.eventMap.put("pickup_cone_floor", Commands.print("PICKUP CONE FLOOR"));
+        Constants.AutoConstants.eventMap.put("pickup_cube_floor", null);
+        Constants.AutoConstants.eventMap.put("pickup_cone_double_substation", null);
+        Constants.AutoConstants.eventMap.put("pickup_cube_double_substation", null);
+        Constants.AutoConstants.eventMap.put("pickup_cone_single_substation", null);
+        Constants.AutoConstants.eventMap.put("pickup_cube_single_substation", null);
+        Constants.AutoConstants.eventMap.put("place_cone_bottom", null);
+        Constants.AutoConstants.eventMap.put("place_cube_bottom", null);
+        Constants.AutoConstants.eventMap.put("place_cone_mid", null);
+        Constants.AutoConstants.eventMap.put("place_cube_mid", null);
+        Constants.AutoConstants.eventMap.put("place_cone_top", null);
+        Constants.AutoConstants.eventMap.put("place_cube_top", null);
+        Constants.AutoConstants.eventMap.put("run_claw", Commands.run(() -> clawSubsystem.set(0.2), clawSubsystem));
+    }
 }
