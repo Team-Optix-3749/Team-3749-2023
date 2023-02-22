9e7a336aecb730e1a7d9fd284f8c7ecf480ece44
Raadwan Masum <piraadwan@gmail.com>
2023-02-21 17:56:11 -0800

    conver arm default command to follow a few points

--- Team 3749 2023/src/main/java/frc/robot/commands/arm/ArmFollowTrajectory.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/arm/ArmFollowTrajectory.java
@@ -1,8 +1,6 @@
 package frc.robot.commands.arm;
 
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.Trajectory.State;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.utils.Constants;
@@ -12,33 +10,46 @@ public class ArmFollowTrajectory extends CommandBase {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
     private final Arm arm;
-    private final Trajectory trajectory;
-    private final Timer timer = new Timer();
+    private final Translation2d[] waypoints;
     
+    private int current_waypoint_index = 0;
+    private Translation2d current_waypoint;
 
-    public ArmFollowTrajectory(Arm arm, Trajectory trajectory) {
+    public ArmFollowTrajectory(Arm arm, Translation2d[] waypoints) {
         this.arm = arm;
-        this.trajectory = trajectory;
+        this.waypoints = waypoints;
 
         addRequirements(arm);
     }
 
     @Override
     public void initialize() {
-        timer.reset();
-        timer.start();
+        current_waypoint = waypoints[0];
     }
 
     @Override
     public void execute() {
 
-        double cur_time = timer.get();
-        State desiredState = trajectory.sample(cur_time);
+        // will stay at the same waypoint if it is the last one
+        if (current_waypoint_index != waypoints.length-1){
+            // if the arm is sufficiantly close, go to the next waypoint
+            if (Constants.withinMargin(0.1,  arm.getArmCoordinate(), current_waypoint)){
+                current_waypoint_index +=1;
+                current_waypoint = waypoints[current_waypoint_index];
+            }
+        }
 
-        arm.setArmPosition(desiredState.poseMeters.getTranslation());
-        
+        arm.setArmPosition(current_waypoint);
+        SmartDashboard.putNumber("CURRENT WAYPOINT X", current_waypoint.getX());
+        SmartDashboard.putNumber("CURRENT WAYPOINT Y", current_waypoint.getY());
+
+        SmartDashboard.putNumber("Arm Coordinate X", arm.getArmCoordinate().getX());
+        SmartDashboard.putNumber("Arm Coordinate Y", arm.getArmCoordinate().getY());
+        SmartDashboard.putBoolean("MARGIN", Constants.withinMargin(0.1,  arm.getArmCoordinate(), current_waypoint));
+        SmartDashboard.putBoolean("INDEX", current_waypoint_index != waypoints.length-1);
+
+        // arm.setArmPosition(new Translation2d(0.75, 0.6));
         // SmartDashboard.putNumberArray("Trajectory Pos", new double[] {desiredState.poseMeters.getTranslation().getX(),desiredState.poseMeters.getTranslation().getY()});
-        System.out.println(desiredState.poseMeters.getTranslation().getX());
         // System.out.println(desiredState.poseMeters.getTranslation().getY());
 
     }
