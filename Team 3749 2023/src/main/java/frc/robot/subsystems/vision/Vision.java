eb2ae84d9c2a169e8a91d602218bac3a5af26dbb
rohin <rs.rohinsood@gmail.com>
2023-02-23 17:32:02 -0800

    fix vision subsystem

--- Team 3749 2023/src/main/java/frc/robot/subsystems/vision/Vision.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -3,17 +3,14 @@ package frc.robot.subsystems.vision;
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
 
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.utils.Constants;
 
 public class Vision extends SubsystemBase {
 
@@ -43,28 +40,22 @@ public class Vision extends SubsystemBase {
         return target.getYaw();
     }
 
-    // The pitch of the target in degrees (positive up)
     public double getPitch(PhotonTrackedTarget target) {
         return target.getPitch();
     }
 
-    // The area (how much of the camera feed the bounding box takes up) as a percent
-    // (0-100)
     public double getArea(PhotonTrackedTarget target) {
         return target.getArea();
     }
 
-    // The skew of the target in degrees (counter-clockwise positive)
     public double getSkew(PhotonTrackedTarget target) {
         return target.getSkew();
     }
 
-    // The 4 corners of the minimum bounding box rectangle
     public List<TargetCorner> getBoundingCorners(PhotonTrackedTarget target) {
         return target.getDetectedCorners();
     }
 
-    // Get id of tag
     public int getTargetId(PhotonTrackedTarget target) {
         return target.getFiducialId();
     }
