9f690730b515d0db755a78f10db65e012ae44048
Raadwan Masum <piraadwan@gmail.com>
2023-02-24 12:44:14 -0800

    create gradient on line plot

--- Team 3749 2023/plot_trajectory.py
+++ Team 3749 2023/plot_trajectory.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
@@ -10,6 +12,20 @@ headers = ['x', 'y']
 
 df = pd.read_csv('data.csv', names=headers)
 
-df.set_index('x').plot()
+x = df.set_index('y')
+y = df.set_index('x')
+cols = np.linspace(0,1,len(x))
+
+points = np.array([x, y]).T.reshape(-1, 1, 2)
+segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+fig, ax = plt.subplots()
+lc = LineCollection(segments, cmap='viridis')
+lc.set_array(cols)
+lc.set_linewidth(2)
+line = ax.add_collection(lc)
+fig.colorbar(line,ax=ax)
+
+plt.scatter(x, y, marker='')
 
 plt.show()
