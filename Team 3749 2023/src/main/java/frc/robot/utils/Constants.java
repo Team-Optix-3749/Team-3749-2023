acd5c7fb1f7814c9ae5d583661ff5363991c0142
rohin <rs.rohinsood@gmail.com>
2023-03-01 14:25:31 -0800

    remove duplicate method

--- Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
@@ -45,21 +45,6 @@ public class Constants {
         return false;
     }
 
-    /***
-     * 
-     * @param margin how close the values need to be to return true. Use a positive
-     *               number
-     * @param a      the first number
-     * @param b      the second number
-     * @return true if it is within the margin, false if not
-     */
-    public static boolean withinMargin(double margin, double a, double b) {
-        if (a + margin >= b && a - margin <= b) {
-            return true;
-        }
-        return false;
-    }
-
     public static final class Claw {
         public static final int claw_id = 22;
     }
