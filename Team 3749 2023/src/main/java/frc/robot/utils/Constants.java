cdb2394f2fc9928a9ae69a5336b641f8658f5724
Raadwan Masum <piraadwan@gmail.com>
2023-02-28 18:10:03 -0800

    increase arm acceleartion

--- Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
@@ -107,7 +107,7 @@ public class Constants {
         // public static final double maxSpeedMPS = 2;
         // public static final double maxAccelerationMPS = 2;
                 public static final double maxSpeedMPS = 5;
-        public static final double maxAccelerationMPS = 5;
+        public static final double maxAccelerationMPS = 10;
 
         public static enum ShoulderSetpoints {
             ZERO(0),
