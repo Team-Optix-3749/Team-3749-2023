b67de4511c62c6c18ebd93066c0cb348cb9f013c
Raadwan Masum <piraadwan@gmail.com>
2023-03-05 19:45:37 -0800

    adjust new DS waypoints

--- Team 3749 2023/src/main/java/frc/robot/subsystems/arm/ArmTrajectories.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/arm/ArmTrajectories.java
@@ -151,7 +151,7 @@ public class ArmTrajectories {
     public static Trajectory getDoubleSubstationTrajectory(boolean isReversed) {
         Pose2d[] waypoints = new Pose2d[] {
                 new Pose2d(0.3, -0.2, new Rotation2d(Math.PI / 4)),
-                new Pose2d(0.75, 0.75, new Rotation2d(Math.PI / 2)),
+                new Pose2d(0.5, 0.75, new Rotation2d(Math.PI / 2)),
         };
 
         return createTrajectory(waypoints, isReversed);
