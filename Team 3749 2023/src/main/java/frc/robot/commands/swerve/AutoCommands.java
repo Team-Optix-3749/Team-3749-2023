47563e9bff188978d1faf55451de8258d90dcc8b
NoahSimon8 <noah.simon9806@gmail.com>
2023-03-07 21:00:42 -0800

    cleaning in the autocommands file

--- Team 3749 2023/src/main/java/frc/robot/commands/swerve/AutoCommands.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/swerve/AutoCommands.java
@@ -1,31 +1,15 @@
 package frc.robot.commands.swerve;
 
-import java.util.List;
-
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
-
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
-
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.commands.arm.MoveArm;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.intake.ArmIntake;
@@ -39,13 +23,9 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
  *         A class to return command sequences for auto
  *         Path Planner is used to create sequences, see docs below
  *         {@link} https://github.com/mjansen4857/pathplanner/wiki/PathPlannerLib:-Java-Usage
- * 
  */
-
 public final class AutoCommands {
-
     /***
-     * 
      * @param swerveSubsystem the subsystem object. Do not make a new instance
      * @param trajectory      a viable trajectory object containing information
      *                        about where the robot should go
@@ -54,7 +34,6 @@ public final class AutoCommands {
      * @return a SwerveControllerCommand based on the trajectory
      * @summary takes a trajectory and moves on it
      */
-
     public static Command followTrajectoryCommand(PathPlannerTrajectory traj, boolean isFirstPath,
             Swerve swerveSubsystem) {
         return new SequentialCommandGroup(
@@ -80,81 +59,6 @@ public final class AutoCommands {
                                // Optional, defaults to true
                         swerveSubsystem // Requires this drive subsystem
                 ));
-    }
-
-    // Use parrelel command groups to run subsystems in parallel
-
-    /***
-     * 
-     * @param swerveSubsystem the subsystem object. Do not make a new instance
-     * @param trajectory      a viable trajectory object containing information
-     *                        about where the robot should go
-     * @return a SwerveControllerCommand based on the trajectory
-     * @summary takes a trajectory and moves on it. Non path planner version
-     */
-    private static Command createCommandFromSwerveTrajectory(Swerve swerveSubsystem,
-            Trajectory trajectory) {
-        PIDController xController = new PIDController(0.1, 0, 0);
-        PIDController yController = new PIDController(0.1, 0, 0);
-
-        ProfiledPIDController thetaController = new ProfiledPIDController(0.5, 0, 0,
-                new TrapezoidProfile.Constraints(
-                        Constants.DriveConstants.kAutoDriveMaxAngularSpeedRadiansPerSecond,
-                        Constants.DriveConstants.kAutoDriveMaxAngularAccelerationUnitsPerSecond));
-        thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-        SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(trajectory,
-                swerveSubsystem::getPose,
-                Constants.DriveConstants.kDriveKinematics, xController, yController, thetaController,
-                swerveSubsystem::setModuleStates, swerveSubsystem);
-        return swerveControllerCommand;
-    }
-
-    /***
-     * @summary takes in several translation 2D's and a relative end rotation.
-     *          Creates a trajectory object. For non path planner use
-     * @param waypoints   an array of translation 2D's. Each coordinate will be hit
-     *                    in the order they are listed. The first should be 0,0
-     * @param endRotation the relative-to-start rotation of the robot by the end of
-     *                    the trajectory
-     * @return a trajectory with the specified coordinates and end rotation
-     */
-    public static Trajectory createTrajectory(Translation2d[] waypoints, double endRotation) {
-        // start and end coordinates, taken from waypoints
-        double[] startXY = new double[] { waypoints[0].getX(), waypoints[0].getY() };
-        double[] endXY = new double[] { waypoints[waypoints.length - 1].getX(),
-                waypoints[waypoints.length - 1].getY() };
-
-        Translation2d[] midpoints = new Translation2d[waypoints.length - 2];
-        for (int i = 1; i < waypoints.length - 1; i++) {
-            midpoints[i - 1] = waypoints[i];
-        }
-
-        TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
-                Constants.DriveConstants.kPhysicalMaxSpeedMetersPerSecond,
-                Constants.DriveConstants.kAutoDriveMaxAccelerationUnitsPerSecond);
-
-        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-                new Pose2d(startXY[0], startXY[1], new Rotation2d(0)),
-                List.of(midpoints),
-                new Pose2d(endXY[0], endXY[1], Rotation2d.fromDegrees(endRotation)),
-                trajectoryConfig);
-
-        return trajectory;
-    }
-
-    // A test method to outline a 1 by 1 meter square while rotating in a circle
-    public static Command getTestSwerveCommand(Swerve swerveSubsystem) {
-        Trajectory trajectory = createTrajectory(
-                new Translation2d[] {
-                        new Translation2d(0, 0),
-                        new Translation2d(Units.feetToMeters(5), 0),
-                }, 0);
-
-        return new SequentialCommandGroup(
-                new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())),
-                createCommandFromSwerveTrajectory(swerveSubsystem, trajectory),
-                new InstantCommand(() -> swerveSubsystem.stopModules()));
     }
 
     // Essentially the template of a getPath command we should be using.
