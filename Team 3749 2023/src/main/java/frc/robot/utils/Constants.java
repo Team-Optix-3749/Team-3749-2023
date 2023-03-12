8448bc50cfe088acb3ac8bcdb688bb2ab93ab609
rohin <rs.rohinsood@gmail.com>
2023-03-12 13:26:53 -0700

    set leds length

--- Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
@@ -218,7 +218,7 @@ public class Constants {
 
     public static class LEDs {
         public static final int pwm_port = 0;
-        public static final int length = 0;
+        public static final int length = 52 + 36;
         public static enum LEDsPattern {RAINBOW, RED, BLUE, GREEN, WHITE, BOUNCE, TWINKLE, NOTHING};
     }
 }
