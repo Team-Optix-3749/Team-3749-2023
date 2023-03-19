971ee571fab15c6c1fa136b5d151e036080189ea
NoahSimon8 <noah.simon9806@gmail.com>
2023-03-19 16:08:12 -0700

    April Tag Alignment DONE

--- Team 3749 2023/src/main/java/frc/robot/commands/swerve/ApriltagAlign.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/swerve/ApriltagAlign.java
@@ -43,7 +43,7 @@ public class ApriltagAlign extends CommandBase {
     private final ProfiledPIDController turnController = new ProfiledPIDController(
             0.0, 0.0, 0.0, new TrapezoidProfile.Constraints(0.0, 0.0));
             
-    private SmartData<Double> drivekP = new SmartData<Double>("Driving KP", 1.5); //2
+    private SmartData<Double> driveKP = new SmartData<Double>("Driving KP", 2);
     private SmartData<Double> turnKP = new SmartData<Double>("Turning KP", 2.6);
 
     private SmartData<Double> driveTolerance = new SmartData<Double>("Driving tolerance", 0.0); // 0.1
@@ -74,6 +74,9 @@ public class ApriltagAlign extends CommandBase {
 
     @Override
     public void execute() {
+
+        driveController.setP(this.driveKP.get());
+        turnController.setP(this.turnKP.get());
     
         if (goalPose == null){
             updateGoalPose();
@@ -146,7 +149,7 @@ public class ApriltagAlign extends CommandBase {
 
     private void updateGoalPose(){
 
-        driveController.setP(drivekP.get());
+        driveController.setP(driveKP.get());
         turnController.setP(turnKP.get());
         
         lastTarget = null;
