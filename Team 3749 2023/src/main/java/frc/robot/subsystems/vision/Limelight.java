cb47484c8ee3544d39830783b37c1bd11bd8989c
rohin <rs.rohinsood@gmail.com>
2023-03-19 22:33:32 -0700

    remove unused widget in vision

--- Team 3749 2023/src/main/java/frc/robot/subsystems/vision/Limelight.java
+++ Team 3749 2023/src/main/java/frc/robot/subsystems/vision/Limelight.java
@@ -26,7 +26,6 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
